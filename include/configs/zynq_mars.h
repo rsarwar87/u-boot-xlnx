86b587bbd62228bec9bb5c70c81b78cc086185e7
Jean Minet <jean.minet@gmail.com>
2017-06-15 11:50:32 +0200

    start mars ZX2 config

--- include/configs/zynq_mars.h
+++ include/configs/zynq_mars.h
@@ -2,14 +2,14 @@
  * (C) Copyright 2013 Xilinx, Inc.
  * (C) Copyright 2015 Antmicro Ltd
  *
- * Configuration for Enclustra Mercury family SOM's
+ * Configuration for Enclustra Mars family SOM's
  * See zynq-common.h for Zynq common configs
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#ifndef __CONFIG_ZYNQ_MERCURY_H
-#define __CONFIG_ZYNQ_MERCURY_H
+#ifndef __CONFIG_ZYNQ_MARS_H
+#define __CONFIG_ZYNQ_MARS_H
 
 #define CONFIG_CPU_FREQ_HZ		800000000
 #define CONFIG_MERCURY_ZX
