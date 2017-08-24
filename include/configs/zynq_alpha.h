91805f8667bbd5781eb35a9dd5d4a1ed8903e24a
Jean Minet <jean.minet@gmail.com>
2017-08-24 23:27:56 +0200

    start alpha from mars

--- include/configs/zynq_alpha.h
+++ include/configs/zynq_alpha.h
@@ -1,15 +1,16 @@
 /*
  * (C) Copyright 2013 Xilinx, Inc.
  * (C) Copyright 2015 Antmicro Ltd
+ * (C) Copyright 2015 Koheron SAS
  *
- * Configuration for Enclustra Mars family SOM's
+ * Configuration for Koheron Alpha
  * See zynq-common.h for Zynq common configs
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#ifndef __CONFIG_ZYNQ_MARS_H
-#define __CONFIG_ZYNQ_MARS_H
+#ifndef __CONFIG_ZYNQ_ALPHA_H
+#define __CONFIG_ZYNQ_ALPHA_H
 
 #define CONFIG_CPU_FREQ_HZ		800000000
 
@@ -27,4 +28,4 @@
 #undef CONFIG_PHY_MARVELL
 #endif
 
-#endif /* __CONFIG_ZYNQ_MERCURY_H */
+#endif /* __CONFIG_ZYNQ_ALPHA_H */
