38cea3e3a23b139a70bd31d2394d59c2e1ee90c9
Jean Minet <jean.minet@gmail.com>
2018-01-18 17:31:24 +0100

    read environment variable from OCM at preboot

--- include/configs/zynq_alpha.h
+++ include/configs/zynq_alpha.h
@@ -9,10 +9,60 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
+#include <configs/zynq-common.h>
+
 #ifndef __CONFIG_ZYNQ_ALPHA_H
 #define __CONFIG_ZYNQ_ALPHA_H
 
-#define CONFIG_CPU_FREQ_HZ		800000000
+#ifdef CONFIG_EXTRA_ENV_SETTINGS
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#endif
+
+#define CONFIG_EXTRA_ENV_SETTINGS	\
+	"kernel_image=uImage\0"	\
+	"kernel_load_address=0x2080000\0" \
+	"ramdisk_image=uramdisk.image.gz\0"	\
+	"ramdisk_load_address=0x4000000\0"	\
+	"devicetree_image=devicetree.dtb\0"	\
+	"devicetree_load_address=0x2000000\0"	\
+	"bitstream_image=system.bit.bin\0"	\
+	"boot_image=BOOT.bin\0"	\
+	"loadbit_addr=0x100000\0"	\
+	"loadbootenv_addr=0x2000000\0" \
+	"kernel_size=0x500000\0"	\
+	"devicetree_size=0x20000\0"	\
+	"ramdisk_size=0x5E0000\0"	\
+	"boot_size=0xF00000\0"	\
+	"fdt_high=0x20000000\0"	\
+	"initrd_high=0x20000000\0"	\
+	"bootenv=uEnv.txt\0" \
+	"loadbootenv=load mmc 0 ${loadbootenv_addr} ${bootenv}\0" \
+	"importbootenv=echo Importing environment from SD ...; " \
+		"env import -t ${loadbootenv_addr} $filesize\0" \
+	"sd_uEnvtxt_existence_test=test -e mmc 0 /uEnv.txt\0" \
+	"preboot="preboot=env import -t 0xFFFFFC00" \
+	"mmc_loadbit=echo Loading bitstream from SD/MMC/eMMC to RAM.. && " \
+		"mmcinfo && " \
+		"load mmc 0 ${loadbit_addr} ${bitstream_image} && " \
+		"fpga load 0 ${loadbit_addr} ${filesize}\0" \
+	"uenvboot=" \
+		"if run loadbootenv; then " \
+			"echo Loaded environment from ${bootenv}; " \
+			"run importbootenv; " \
+		"fi; " \
+		"if test -n $uenvcmd; then " \
+			"echo Running uenvcmd ...; " \
+			"run uenvcmd; " \
+		"fi\0" \
+	"sdboot=if mmcinfo; then " \
+			"run uenvboot; " \
+			"echo Copying Linux from SD to RAM... && " \
+			"load mmc 0 ${kernel_load_address} ${kernel_image} && " \
+			"load mmc 0 ${devicetree_load_address} ${devicetree_image} && " \
+			"load mmc 0 ${ramdisk_load_address} ${ramdisk_image} && " \
+			"bootm ${kernel_load_address} ${ramdisk_load_address} ${devicetree_load_address}; " \
+		"fi\0" \
+#endif
 
 #define CONFIG_ZYNQ_SDHCI0
 
@@ -20,8 +70,6 @@
 #define CONFIG_PHY_MICREL
 
 #define PHY_ANEG_TIMEOUT	8000	/* PHY needs a longer aneg time */
-
-#include <configs/zynq-common.h>
 
 /* Unselect Marvell PHY (selected by zynq-common) */
 #ifdef CONFIG_PHY_MARVELL
