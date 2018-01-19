c20d02da07858b132d4a692e1d8abdc729107540
Jean Minet <jean.minet@gmail.com>
2018-01-19 15:35:12 +0100

    add config file for zynq_te0745

--- include/configs/zynq_te0745.h
+++ include/configs/zynq_te0745.h
@@ -1,16 +1,5 @@
-/*
- * (C) Copyright 2013 Xilinx, Inc.
- * (C) Copyright 2015 Antmicro Ltd
- * (C) Copyright 2017 Koheron SAS
- *
- * Configuration for MYIR MYD-C7Z015 and MYD-C7Z020
- * See zynq-common.h for Zynq common configs
- *
- * SPDX-License-Identifier:	GPL-2.0+
- */
-
-#ifndef __CONFIG_ZYNQ_MYIR_H
-#define __CONFIG_ZYNQ_MYIR_H
+#ifndef __CONFIG_ZYNQ_TE0745_H
+#define __CONFIG_ZYNQ_TE0745_H
 
 #include <configs/zynq-common.h>
 
@@ -55,18 +44,4 @@
 		"fi\0" \
 	"sdboot=echo Importing environment from SD... && mmcinfo && fatload mmc 0 0x2000000 uEnv.txt && env import -t 0x2000000 ${filesize} && boot"
 
-#define CONFIG_CPU_FREQ_HZ		800000000
-
-#define CONFIG_ZYNQ_SDHCI0
-
-/* Select Micrel PHY */
-#define CONFIG_PHY_MICREL
-
-#define PHY_ANEG_TIMEOUT	8000	/* PHY needs a longer aneg time */
-
-/* Unselect Marvell PHY (selected by zynq-common) */
-#ifdef CONFIG_PHY_MARVELL
-#undef CONFIG_PHY_MARVELL
-#endif
-
-#endif /* __CONFIG_ZYNQ_MYIR_H */
+#endif /* __CONFIG_ZYNQ_TE0745_H */
